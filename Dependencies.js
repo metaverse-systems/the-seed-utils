47c82c80b3611506ec383879bc48e86073ca1124
Tim Schwartz <tim@metaverse.systems>
2020-03-14 22:39:04 -0500

    Updated deps

--- Dependencies.js
+++ Dependencies.js
@@ -3,23 +3,45 @@ const fs = require('fs');
 const { execSync } = require('child_process');
 
 const deps = {
-  "native": {
-    "prefix": "/usr",
-    "libs": {
+  "Native": {
+    "Prefix": "",
+    "Prefix directory": "/usr",
+    "Build tools": {
+      "libtoolize": false,
+      "aclocal": false,
+      "autoheader": false,
+      "automake": false,
+      "autoconf": false,
+    },
+    "Compilers": {
+      "g++": false,
+    },
+    "Libraries": {
       "jsoncpp": false,
       "uuid": false,
       "ecs-cpp": false
     },
-    "files": {
+    "Files": {
     }
   },
-  "win64": {
-    "prefix": "/usr/x86_64-w64-mingw32",
-    "libs": {
+  "Win64": {
+    "Prefix": "x86_64-w64-mingw32-",
+    "Prefix directory": "/usr/x86_64-w64-mingw32",
+    "Build tools": {
+      "libtoolize": false,
+      "aclocal": false,
+      "autoheader": false,
+      "automake": false,
+      "autoconf": false,
+    },
+    "Compilers": {
+      "g++": false
+    },
+    "Libraries": {
       "jsoncpp": false,
       "ecs-cpp": false
     },
-    "files": {
+    "Files": {
       "include/mingw.thread.h": false,
       "include/mingw.invoke.h": false,
       "include/mingw.mutex.h": false
@@ -30,23 +52,42 @@ const deps = {
 class Dependencies {
   constructor() {
     Object.keys(deps).forEach(target => {
-      let prefix = deps[target].prefix;
+      let prefix = deps[target].Prefix;
+      let prefixDir = deps[target]["Prefix directory"];
 
-      let pkg_config_path = prefix + "/lib/pkgconfig";
+      let pkg_config_path = prefixDir + "/lib/pkgconfig";
       let pkg_config_command = "PKG_CONFIG_PATH=" + pkg_config_path + " pkg-config --exists ";
 
-      Object.keys(deps[target].libs).forEach(lib => {
+      Object.keys(deps[target].Libraries).forEach(lib => {
         let check_command = pkg_config_command + lib;
         try {
           execSync(check_command);
-          deps[target].libs[lib] = true;
+          deps[target].Libraries[lib] = true;
         } catch(err) {
         }
       });
 
-      Object.keys(deps[target].files).forEach(file => {
-        if(fs.existsSync(prefix + "/" + file)) {
-          deps[target].files[file] = true;
+      Object.keys(deps[target]["Build tools"]).forEach(tool => {
+        let check_command = "which " + tool;
+        try {
+          let result = execSync(check_command).toString();
+          deps[target]["Build tools"][tool] = result;
+        } catch(err) {
+        }
+      });
+
+      Object.keys(deps[target]["Compilers"]).forEach(compiler => {
+        let check_command = "which " + prefix + compiler;
+        try {
+          let result = execSync(check_command).toString();
+          deps[target]["Compilers"][compiler] = result;
+        } catch(err) {
+        }
+      });
+
+      Object.keys(deps[target].Files).forEach(file => {
+        if(fs.existsSync(prefixDir + "/" + file)) {
+          deps[target].Files[file] = true;
         }
       });
     });
